1ced4709b2ee75acbfd7fc3af7d473392e405c62
Vasileios Gakias <vagakias423956@gmail.com>
2019-01-12 16:37:21 +0200

    basic user interface from command line

--- src/main/java/phone_filtering/Main.java
+++ src/main/java/phone_filtering/Main.java
@@ -1,11 +1,33 @@
 package phone_filtering;
 
-public class Main {
+import java.util.Scanner;
 
+import phone_filtering.tools.Validator;
+
+public class Main {
+	private static Scanner sc;
+	private static String input;
+	
 	public static void main(String[] args) {
+		sc=new Scanner(System.in);
 		
+		while(!Validator.inputIsValid(input=getInput())) {
+			printDirectionsForValidInput();
+		}
+		
+		System.out.println("Your input number is: "+input);
+		sc.close();
 	}
 	
+	private static String getInput() {
+		System.out.println("Please enter a telephone number:");
+		return sc.nextLine();
+	}
+	
+	private static void printDirectionsForValidInput() {
+		System.out.println("Your input is not valid");
+		System.out.println("Valid telephone number must be some groups of 1 to 3 digits seperated by one space character");
+	}
 	
 
 }
